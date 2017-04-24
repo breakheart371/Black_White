4f1145d9f46d5ceaa73da9486f513d251a04e32a
babakkhan <babakkhan375@gmail.com>
2017-04-25 00:47:55 +0430

    Add files via upload

--- bot/config.lua
+++ bot/config.lua
@@ -1,1 +1,40 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "supergroup",
+    "msg_checks",
+    "pin",
+    "owner",
+    "online",
+    "plugins",
+    "admin",
+    "del",
+    "clean",
+    "expiretime",
+    "filter",
+    "setlink",
+    "setrules",
+    "me",
+    "like",
+    "rmsg_all",
+    "leave"
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    229445008, --sudo ایدی شما
+    0,
+    0
+  },
+robot = {
+316813335,--bot ایدی ربات
+    0
+  }
+}
+return _
+end
 
+--@sudo_hacker
+--https://github.com/babakkhan375
